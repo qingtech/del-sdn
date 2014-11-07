243d0c819a5349adac8172132b1ea0910be933a0
woniu17 <233769435@qq.com>
2014-11-07 20:45:39 +0800

    1. add script <cd to path of main.sh> to main.sh, this make main.sh can run in any current directory; 2. add directory tmp to put testing code

--- spcd/main.sh
+++ spcd/main.sh
@@ -1,4 +1,34 @@
 #!/bin/sh
+#get the abs path of main.sh
+this_dir=`pwd`
+
+dirname $0|grep "^/" >/dev/null
+
+if [ $? -eq 0 ];then
+
+	this_dir=`dirname $0`
+
+else
+
+	dirname $0|grep "^\." >/dev/null
+
+	retval=$?
+
+	if [ $retval -eq 0 ];then
+
+		this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+
+	else
+
+		this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+
+	fi
+
+fi
+
+echo 'cd to '$this_dir
+cd $this_dir
+
 echo 'run main.py to generate load.txt, traffic.txt'
 python main.py
 echo 'copy load.txt traffic.txt to directory Rscript'
